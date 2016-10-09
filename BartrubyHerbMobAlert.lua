32d75aa32220672ed41cf09486910d5f8117f05b
caliaha <34219663+Caliaha@users.noreply.github.com>
2016-10-09 00:18:41 +0000

    Added custom font option

--- BartrubyHerbMobAlert.lua
+++ BartrubyHerbMobAlert.lua
@@ -13,6 +13,9 @@ function BartrubyHerbMobAlert:OnInitialize()
   profile = {
    alertWait = 10,
    alertSound = "HerbAlert",
+   customFont = false,
+   fontSize = 32,
+   fontType = "Friz Quadrata TT",
   },
  }
  
@@ -25,7 +28,6 @@ end
 
 function BartrubyHerbMobAlert:OnEnable()
  self:RegisterEvent("NAME_PLATE_UNIT_ADDED")
- --self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")  Will trigger on any spawns not just ours
 end
 
 function BartrubyHerbMobAlert:OnDisable()
@@ -36,10 +38,47 @@ function BartrubyHerbMobAlert:NAME_PLATE_UNIT_ADDED(event, unit)
  local _, _, _, _, _, npc_id, _ = strsplit("-", UnitGUID(unit))
 
  if (HERBMOBS[npc_id] and (GetTime() > self.lastAlert)) then
-  UIErrorsFrame:AddMessage((UnitName(unit)) .. " Spawned!")
+  if (self.db.profile.customFont) then
+   self:CustomMessage((UnitName(unit)) .. " Spawned!")
+  else  
+   UIErrorsFrame:AddMessage((UnitName(unit)) .. " Spawned!")
+  end
   PlaySoundFile(LSM:Fetch("sound", self.db.profile.alertSound), "MASTER")
   self.lastAlert = GetTime() + self.db.profile.alertWait
  end
+end
+
+function BartrubyHerbMobAlert:CustomMessage(message)
+ if (not self.message) then
+  self.message = CreateFrame("Frame", nil, UIParent)
+  self.message.text = self.message:CreateFontString(nil, 'OVERLAY')
+  self.message.text:SetAllPoints(true)
+  self.message:SetWidth(200)
+  self.message:SetHeight(40)
+  self.message:SetPoint("BOTTOM", UIErrorsFrame, "TOP", 0, 0)
+  
+  self.message:SetScript("OnShow", function(self)
+									self:SetWidth((self.text:GetStringWidth() or 10) + 10)
+                                   end)
+								   
+  self.message:SetScript("OnUpdate", function(self)
+                                      local now = GetTime()
+									  local percent = (now - self.timeStart) / 7
+									  percent = 1 - percent
+									  
+                                      self:SetAlpha(percent)
+									  if (percent <= 0) then
+									   self:Hide()
+									  end
+                                     end)
+ end
+ self.message:Hide()
+ 
+ self.message.text:SetFont(LSM:Fetch("font", self.db.profile.fontType), self.db.profile.fontSize, "")
+ self.message.text:SetText(message)
+ self.message.timeStart = GetTime()
+ self.message:Show()
+ self.message:SetAlpha(1)
 end
 
 function BartrubyHerbMobAlert:GenerateOptions()
@@ -61,6 +100,24 @@ function BartrubyHerbMobAlert:GenerateOptions()
 	order = 2,
     values = LSM:HashTable("sound"),
     dialogControl = "LSM30_Sound",
+   },
+   customFont = {
+    name = "Use Custom Font",
+	type = "toggle",
+	order = 4,
+   },
+   fontType = {
+    name = "Font",
+    order = 4.1,
+    type = "select",
+    values = LSM:HashTable("font"),
+    dialogControl = "LSM30_Font",
+   },
+   fontSize = {
+    name = "Font Size",
+    type = "range",
+	order = 4.2,
+    min = 8, max = 64, step = 1,
    },
   },
  }
